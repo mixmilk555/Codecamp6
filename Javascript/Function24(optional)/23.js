4ff977c55bbc7af21092fccbb291e75b24fc8140
mixmilk666 <mobmob555@hotmail.co.th>
2020-08-09 20:48:49 +0700

    fix 23

--- Javascript/Function24(optional)/23.js
+++ Javascript/Function24(optional)/23.js
@@ -15,7 +15,7 @@ function draw(n){
         }
         document.write(`<br>`);
     }
-    for (let i = 1; i <= n; i++) {
+    for (let i = 2; i <= n; i++) {
         for (let j = 2; j <= i; j++) {
             document.write(`-`);
         }
